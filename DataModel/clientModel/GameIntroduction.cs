bffe4512c3037151ac9d8926384c22dc591ea420
18550261681 <369057938@qq.com>
2017-04-17 00:35:18 +0800

    123

---
+++ DataModel/clientModel/GameIntroduction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataModel.clientModel
+{
+    public class GameIntroduction : BaseModel
+    {
+        List<string> imageList;
+        /// <summary>
+        /// imageList
+        /// </summary>
+        public List<string> ImageList
+        {
+            get { return imageList; }
+            set { imageList = value; }
+        }
+
+        string title;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        string introduction;
+        /// <summary>
+        /// Introduction
+        /// </summary>
+        public string Introduction
+        {
+            get { return introduction; }
+            set { introduction = value; }
+        }
+    }
+}
---
+++ VRClient/view/winLogin.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using VRClient.viewModel;
+
+namespace VRClient.view
+{
+    /// <summary>
+    /// winLogin.xaml 的交互逻辑
+    /// </summary>
+    public partial class winLogin : Window
+    {
+        ResourceDictionary style;
+
+        public winLogin()
+        {
+            InitializeComponent();
+
+            this.DataContext = new winLoginViewModel();
+
+            this.MouseDown += new MouseButtonEventHandler(winLogin_MouseDown);
+        }
+
+        void winLogin_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.GetPosition(this).Y < 20)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
